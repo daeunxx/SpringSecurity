fe0d2ea264caad69c8c82099b907848c3a9f5568
daeun <86859440+daeunxx@users.noreply.github.com>
2023-06-20 09:30:16 +0900

    SpringSecurity Config 설정

---
+++ src/main/java/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package config;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                    .sessionManagement((sessionManagement) ->
+                            sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    )
+                    .authorizeHttpRequests((authorizeRequests) ->
+                            authorizeRequests.anyRequest().permitAll()
+                    );
+
+        return httpSecurity.build();
+    }
+}
